0a4209bb73873ea0b139d14b965a64804b2b30d2
Stephanie <63614558+sdelacruz7@users.noreply.github.com>
2020-06-11 11:02:27 -0700

    Create form.php

---
+++ form.php
@@ -0,0 +1,14 @@
+<?php
+if(){//show data
+
+}else{//shor form
+  echo 
+  '
+  <form action="">
+  <p>First Name <input type="text" name="FirstName" /></p>
+  <p><input type="submit"/></p>
+  
+  </form>
+  ';
+}
+?>
